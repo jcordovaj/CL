137825b52a7cf98a075f4100df6f37972e42925f
Jorge Cordova <55065873+jcordovaj@users.noreply.github.com>
2023-06-22 16:34:47 -0400

    cambia main
    test sólo funciones que retornan un print

--- main.py
+++ main.py
@@ -1,81 +1,56 @@
-import pandas as pd
-import openpyxl
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 
-# Cargar los datos desde 'xlsx'
-df_work = pd.read_excel('dfwork.xlsx', sheet_name='Sheet1')
-############ F U N C I O N E S ###########
-def cantidad_filmaciones_mes(v_Mes):
-    return 'test pelis mes'
+@app.get('/cantidad_filmaciones_mes/{mes}')
+def cantidad_filmaciones_mes(mes: str):
+    '''Se ingresa el mes y la función retorna la cantidad de películas que se estrenaron
+       ese mes históricamente'''
+    # respuesta = # Lógica para obtener la cantidad de películas por mes
+    return {'mes': mes, 'cantidad': respuesta}
 
-def cantidad_filmaciones_dia(v_Dia):
-    return 'test pelis dia'
+@app.get('/cantidad_filmaciones_dia/{dia}')
+def cantidad_filmaciones_dia(dia: str):
+    '''Se ingresa el día y la función retorna la cantidad de películas que se estrenaron ese día históricamente'''
+    # respuesta = # Lógica para obtener la cantidad de películas por día
+    #return {'dia': dia, 'cantidad': respuesta}
+    return print('prueba films x día')
 
-def score_titulo(v_nom_pel):
-    return 'test score'
+@app.get('/score_titulo/{titulo}')
+def score_titulo(titulo: str):
+    '''Se ingresa el título de una filmación esperando como respuesta el título, el año de estreno y el score'''
+    #respuesta = # Lógica para obtener el score de un título
+    #return {'titulo': titulo, 'anio': respuesta, 'popularidad': respuesta}
+    return print('prueba score')
 
-def votos_titulo(v_titulo):
-    return 'test votos'        
+@app.get('/votos_titulo/{titulo}')
+def votos_titulo(titulo: str):
+    '''Se ingresa el título de una filmación esperando como respuesta el título, la cantidad de votos y el valor promedio de las votaciones. 
+    La misma variable deberá de contar con al menos 2000 valoraciones, 
+    caso contrario, debemos contar con un mensaje avisando que no cumple esta condición y que por ende, no se devuelve ningún valor.'''
+    #respuesta = # Lógica para obtener los votos de un título
+    #return {'titulo': titulo, 'anio': respuesta, 'voto_total': respuesta, 'voto_promedio': respuesta}
+    return print('prueba votos')
 
-def get_actor(v_nom_actor):
-    return 'test actor'
+@app.get('/get_actor/{nombre_actor}')
+def get_actor(nombre_actor: str):
+    '''Se ingresa el nombre de un actor que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. 
+    Además, la cantidad de películas en las que ha participado y el promedio de retorno'''
+    #respuesta = # Lógica para obtener información del actor
+    #return {'actor': nombre_actor, 'cantidad_filmaciones': respuesta, 'retorno_total': respuesta, 'retorno_promedio': respuesta}
+    return print('prueb actor')
 
-def get_director(v_nom_dire):
-    return 'test director'
+@app.get('/get_director/{nombre_director}')
+def get_director(nombre_director: str):
+    ''' Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. 
+    Además, deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma.'''
+    #respuesta = # Lógica para obtener información del director
+    #return {'director': nombre_director, 'retorno_total_director': respuesta, 'peliculas': respuesta, 'anio': respuesta, 'retorno_pelicula': respuesta,     #'budget_pelicula': respuesta, 'revenue_pelicula': respuesta}
+    return print('test dire')
 
-def recomendacion(gustos):
-    return 'test director'
-
-def open_readme():
-    return 'test readme'
-
-@app.get("/")
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-@app.get("/consulta")
-async def consulta(request: Request, opcion: int):
-    if opcion == 1:
-        v_Mes = input("Ingrese un mes a consultar (Ejemplo: Enero, Febrero): ")
-        cantidad_filmaciones_mes(v_Mes)
-        return {"message": "Consulta realizada pel x mes"}
-    elif opcion == 2:
-        v_Dia = input("Ingrese el día a consultar (Ejemplo: Lunes, Martes): ")
-        cantidad_filmaciones_dia(v_Dia)
-        return {"message": "Consulta realizada pel x día"}
-    elif opcion == 3:
-        v_nom_pel = input("Ingrese el nombre de una película (Ejemplo: 'Star Wars', 'Casablanca'): ")
-        score_titulo(v_nom_pel)
-        return {"message": "Consulta realizada pel score"}
-    elif opcion == 4:
-        v_titulo = input("Ingrese el nombre de una película (Ejemplo: 'Jumanji', 'Grease'): ")
-        votos_titulo(v_titulo)
-        return {"message": "Consulta realizada votos"}
-    elif opcion == 5:
-        v_nom_actor = input("Ingrese el nombre de un actor (Ejemplo: 'Tom Hanks', 'Frank Sinatra'): ")
-        get_actor(v_nom_actor)
-        return {"message": "Consulta realizada actor"}
-    elif opcion == 6:
-        v_nom_dire = input("Ingrese el nombre de un director (Ejemplo: 'Tom Hanks', 'Frank Capra'): ")
-        get_director(v_nom_dire)
-        return {"message": "Consulta realizada director"}
-    elif opcion == 7:
-        gustos = input("Ingrese su gusto en pelis o nombre de peli: ")
-        recomendacion(gustos)
-        return {"message": "Consulta realizada recomendación"}
-    elif opcion == 8:
-        open_readme()
-        return {"message": "Consulta realizada"}
-  
-    else:
-        return {"message": "Opción no válida"}
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+@app.get('/recomendacion/{titulo}')
+def recomendacion(titulo: str):
+    '''Ingresas un nombre de película y te recomienda las similares en una lista'''
+    respuesta = # Lógica para obtener la lista de recomendaciones
+    #return {'lista_recomendada': respuesta}
+    return print('test recomenda')
